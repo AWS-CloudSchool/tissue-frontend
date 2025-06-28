ff4880410ed58900ca7fe085adf053ee87dde7fa
eungyo <eungyo0965@gmail.com>
2025-06-28 09:10:27 +0200

    feat(dashboard) : final css

--- src/pages/DashboardPage/DashboardPage.js
+++ src/pages/DashboardPage/DashboardPage.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import AuroraBackground from '../../components/AuroraBackground/AuroraBackground';
 import TopBar from '../../components/TopBar/TopBar';
 import Footer from '../../components/Footer/Footer';
 import axios from 'axios';
